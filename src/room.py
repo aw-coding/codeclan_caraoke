86fecbcccf23570343c12b2d1f90252c4f7827a2
Andrew Wood <andrewwood90@hotmail.com>
2020-11-29 13:08:06 +0000

    seven tests passed

--- src/room.py
+++ src/room.py
@@ -1,6 +1,13 @@
+
+
+
 class Room():
     def __init__(self, name, capacity):
         self.name = name
         self.set_list = []
         self.capacity = capacity
         self.current_occupants = []
+
+
+    def add_song_to_set_list(self, item):
+        self.set_list.append(item)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,12 +1,23 @@
 import unittest
 
 from src.room import Room
+from src.song import Song
+
+
+#Room(name, setlist[], capacity, current occupants[])
 
 class TestRoom(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): #setUp can only contain Room objects!
         self.large_room = Room("Large Room", 5)
         self.small_room = Room("Small Room", 3)
         self.vip_room = Room("VIP Room", 8)
+
+        song_1 = Song("Move on Up")
+        self.song_2 = Song("I Want To Break Free")
+        self.song_3 = Song("Africa")
+        self.song_4 = Song("September")
+        self.song_5 = Song("Life on Mars")
+        self.song_6 = Song("Virginia Plain")
 
 
     def test_room_has_name(self):
@@ -14,5 +25,19 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_capacity(self):
         self.assertEqual(5, self.large_room.capacity)
+
+    def test_add_song_to_room_set_list(self):
+        song_1 = Song("Move on Up")
+        self.small_room.add_song_to_set_list(song_1)
+        self.assertEqual("Move on Up", self.small_room.set_list[0].name)
+
+    def test_add_three_songs_to_set_list(self):
+        song_1 = Song("Move on Up")
+        song_2 = Song("I Want To Break Free")
+        song_3 = Song("Africa")
+        self.small_room.add_song_to_set_list(song_1)
+        self.small_room.add_song_to_set_list(song_2)
+        self.small_room.add_song_to_set_list(song_3)
+        self.assertEqual([song_1, song_2, song_3], self.small_room.set_list)
 
 
