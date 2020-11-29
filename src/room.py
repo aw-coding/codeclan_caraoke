2ff91c258937ee2e4623f3923f2978aa1c603f66
Andrew Wood <andrewwood90@hotmail.com>
2020-11-29 12:26:19 +0000

    three tests passed

--- src/room.py
+++ src/room.py
@@ -1,6 +1,6 @@
 class Room():
-    def __init__(self, name, set_list, capacity):
+    def __init__(self, name, capacity):
         self.name = name
-        self.set_list = set_list
+        self.set_list = []
         self.capacity = capacity
         self.current_occupants = []
---
+++ tests/room_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.room import Room
+
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.large_room = Room("Large Room", 5)
+        self.small_room = Room("Small Room", 3)
+        self.vip_room = Room("VIP Room", 8)
+
+
+    def test_room_has_name(self):
+        self.assertEqual("Large Room", self.large_room.name)
+
+
